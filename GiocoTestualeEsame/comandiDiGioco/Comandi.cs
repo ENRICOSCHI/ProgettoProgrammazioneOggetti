359b5ae68e62e9f93f440a6e5911b2352ffa6dad
ENRICO <fenrico472@gmail.com>
2025-04-08 13:29:42 +0200

    creata classe comandi

---
+++ GiocoTestualeEsame/comandiDiGioco/Comandi.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiocoTestualeEsame.comandiDiGioco
+{
+    public class Comandi
+    {
+        public static void ControlloComandi(string comando)
+        {
+            /*swtich case con tutti i casi del comando che può esser stato scelto*/
+        }
+    }
+}
